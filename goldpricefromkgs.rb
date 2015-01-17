dec953c9009153af0646beddf18272ebeb01f85c
Kyle Cushing <kyle.cushing@clearnda.com>
2015-01-17 21:18:14 +0000

    Gold Prices

---
+++ goldpricefromkgs.rb
@@ -0,0 +1,5 @@
+def price_of_gold_from_kg(kg)
+  price_of_gold = 1336
+  ounces = kg*16
+return ounces*price_of_gold
+end
---
+++ goldpricefrompounds.rb
@@ -0,0 +1,5 @@
+def price_of_gold_from_pounds(pounds)
+  price_of_gold = 1336
+  ounces = pounds*16
+return ounces*price_of_gold
+end
